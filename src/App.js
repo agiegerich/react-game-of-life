1926f856ead74ab23e3af0ff56abb504132d1b53
Albert Giegerich <albert.a.giegerich@gmail.com>
2019-09-15 14:58:46 -0500

    Generate row divs based on height prop.

--- src/App.js
+++ src/App.js
@@ -3,12 +3,26 @@ import logo from './logo.svg';
 import './App.css';
 
 
-
 class App extends React.Component {
+  constructor(props) {
+    super(props);
 
+    const grid = [];
+    for (let i = 0; i < props.height; i++) {
+      grid.push(Array(1).fill(null));
+    }
+
+    this.state = {
+      grid: grid,
+    }
+  }
   render() {
+    const rows = this.state.grid.map((row, index) => {
+      return (<div key={index} className="row">Test</div>);
+    });
     return (
         <div className="App">
+          {rows}
         </div>
     );
   }
